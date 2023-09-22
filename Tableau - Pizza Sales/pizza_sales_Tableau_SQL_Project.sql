8c0bb84ed967213a18dada22ce6b0297ca1b3b0b
Akshay Paunikar <akshayakki29@gmail.com>
2023-09-22 14:37:58 +0530

    Tableau - Pizza Sales - KPI related queries completed

--- Tableau - Pizza Sales/pizza_sales_Tableau_SQL_Project.sql
+++ Tableau - Pizza Sales/pizza_sales_Tableau_SQL_Project.sql
@@ -6,6 +6,23 @@ SELECT * FROM pizzasales;
 
 /* Q1. Find out the total revenue */
 SELECT ROUND(SUM(total_price),2) AS total_revenue FROM pizzasales;
+-- ANSWER -- '817860.05'
 
-/* Q2. */
+/* Q2. What is the average order value ? */
+SELECT ROUND((SUM(total_price)/COUNT(DISTINCT(order_id))),2) AS average_order_value FROM pizzasales;
+-- ANSWER -- '38.31'
+
+/* Q3. Find out the total number of pizza's sold */
+SELECT SUM(quantity) AS total_pizza_sold FROM pizzasales;
+-- ANSWER -- '49574'
+
+/* Q4. Find out the total number of orders placed */
+SELECT COUNT(DISTINCT(order_id)) AS total_orders_placed FROM pizzasales;
+-- ANSWER -- '21350'
+
+/* Q5. Find out the average number of pizza's sold per order placed */
+SELECT ROUND((SUM(quantity) / COUNT(DISTINCT(order_id))),2) AS avg_num_pizza_sold_per_order FROM pizzasales;
+-- ANSWER -- '2.32'
+
+-- KPI's QUERY COMPLETED --
 
