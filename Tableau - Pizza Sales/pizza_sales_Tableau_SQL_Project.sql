88881daf5ce074b2e784225d1d5c1e00fb19d886
Akshay Paunikar <akshayakki29@gmail.com>
2023-09-22 18:06:01 +0530

    Tableau - Pizza Sales - Chart related queries completed

--- Tableau - Pizza Sales/pizza_sales_Tableau_SQL_Project.sql
+++ Tableau - Pizza Sales/pizza_sales_Tableau_SQL_Project.sql
@@ -6,23 +6,23 @@ SELECT * FROM pizzasales;
 
 /* Q1. Find out the total revenue */
 SELECT ROUND(SUM(total_price),2) AS total_revenue FROM pizzasales;
--- ANSWER -- '817860.05'
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q2. What is the average order value ? */
 SELECT ROUND((SUM(total_price)/COUNT(DISTINCT(order_id))),2) AS average_order_value FROM pizzasales;
--- ANSWER -- '38.31'
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q3. Find out the total number of pizza's sold */
 SELECT SUM(quantity) AS total_pizza_sold FROM pizzasales;
--- ANSWER -- '49574'
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q4. Find out the total number of orders placed */
 SELECT COUNT(DISTINCT(order_id)) AS total_orders_placed FROM pizzasales;
--- ANSWER -- '21350'
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q5. Find out the average number of pizza's sold per order placed */
 SELECT ROUND((SUM(quantity) / COUNT(DISTINCT(order_id))),2) AS avg_num_pizza_sold_per_order FROM pizzasales;
--- ANSWER -- '2.32'
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 -- KPI's QUERY COMPLETED --
 
@@ -36,7 +36,7 @@ SELECT HOUR(STR_TO_DATE(order_time,'%T')) AS order_hours,
 FROM pizzasales
 GROUP BY order_hours
 ORDER BY order_hours ASC;
--- ANSWER -- exported to a csv file with name "hourly trend of the total pizza's sold, total orders placed & total revenue generated"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q2. What is the daily trend of the total pizza's sold, total orders placed & total revenue generated - day name */
 SELECT WEEKDAY(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_day_number,
@@ -47,7 +47,7 @@ SELECT WEEKDAY(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_day_number,
 FROM pizzasales
 GROUP BY order_day_number
 ORDER BY order_day_number;
--- ANSWER -- exported to a csv file with name "daily trend of the total pizza's sold, total orders placed & total revenue generated - day name"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q3. What is the daily trend of the total pizza's sold, total orders placed & total revenue generated - day of month */
 SELECT DAYOFMONTH(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_day_of_month,
@@ -57,7 +57,7 @@ SELECT DAYOFMONTH(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_day_of_month,
 FROM pizzasales
 GROUP BY order_day_of_month
 ORDER BY order_day_of_month;
--- ANSWER -- exported to a csv file with name "daily trend of the total pizza's sold, total orders placed & total revenue generated - day of month"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q4. What is the weekly trend of the total pizza's sold, total orders placed & total revenue generated */
 SELECT WEEKOFYEAR(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_week,
@@ -68,7 +68,7 @@ SELECT WEEKOFYEAR(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_week,
 FROM pizzasales
 GROUP BY order_week, order_year
 ORDER BY order_week, order_year ASC;
--- ANSWER -- exported to a csv file with name "weekly trend of the total pizza's sold, total orders placed & total revenue generated"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q5. What is the monthly trend of the total pizza's sold, total orders placed & total revenue generated */
 SELECT MONTH(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_month_number,
@@ -80,7 +80,7 @@ SELECT MONTH(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_month_number,
 FROM pizzasales
 GROUP BY order_month_number, order_month_name, order_year
 ORDER BY order_month_number, order_month_name, order_year ASC;
--- ANSWER -- exported to a csv file with name "monthly trend of the total pizza's sold, total orders placed & total revenue generated"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q6. What is the quarterly trend of the total pizza's sold, total orders placed & total revenue generated */
 SELECT QUARTER(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_quarter_number,
@@ -91,7 +91,7 @@ SELECT QUARTER(STR_TO_DATE(order_date,'%d-%m-%Y')) AS order_quarter_number,
 FROM pizzasales
 GROUP BY order_quarter_number, order_year
 ORDER BY order_quarter_number, order_year ASC;
--- ANSWER -- exported to a csv file with name "quarterly trend of the total pizza's sold, total orders placed & total revenue generated"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q7. What is the total orders, total quantity sold, total revenue generated & percentage of pizza sales by pizza category */
 SELECT pizza_category,
@@ -102,7 +102,7 @@ SELECT pizza_category,
 FROM pizzasales
 GROUP BY pizza_category
 ORDER BY percent_pizza_sales DESC;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated & percentage of pizza sales by pizza category"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q8. What is the total orders, total quantity sold, total revenue generated & percentage of pizza sales by pizza size */
 SELECT pizza_size,
@@ -113,7 +113,7 @@ SELECT pizza_size,
 FROM pizzasales
 GROUP BY pizza_size
 ORDER BY percent_pizza_sales DESC;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated & percentage of pizza sales by pizza size"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q9. What is the total orders, total quantity sold, total revenue generated & percentage of pizza sales by pizza category & pizza size */
 SELECT pizza_category,
@@ -124,7 +124,7 @@ SELECT pizza_category,
     ROUND((SUM(total_price) * 100)/ (SELECT SUM(total_price) FROM pizzasales),2) AS percent_pizza_sales
 FROM pizzasales
 GROUP BY pizza_category, pizza_size;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated & percentage of pizza sales by pizza category & pizza size"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /*NOTE: We can use WHERE clause in above three queries to get monthly, quarterly, daily, hourly data also if required 
 in order to do that add the same WHERE clause to the SELECT statement in the parenthesis also 
@@ -147,7 +147,7 @@ SELECT pizza_name,
 FROM pizzasales
 GROUP BY pizza_name
 ORDER BY total_revenue DESC LIMIT 5;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated categorized by pizza name - Top 5 by total revenue"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q11. What is the total orders, total quantity sold, total revenue generated categorized by pizza name - Bottom 5 by total revenue */
 SELECT pizza_name,
@@ -157,7 +157,7 @@ SELECT pizza_name,
 FROM pizzasales
 GROUP BY pizza_name
 ORDER BY total_revenue ASC LIMIT 5;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated categorized by pizza name - Bottom 5 by total revenue"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q12. What is the total orders, total quantity sold, total revenue generated categorized by pizza name - Top 5 by quantity sold */
 SELECT pizza_name,
@@ -167,7 +167,7 @@ SELECT pizza_name,
 FROM pizzasales
 GROUP BY pizza_name
 ORDER BY total_pizza_sold DESC LIMIT 5;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated categorized by pizza name - Top 5 by quantity sold"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q13. What is the total orders, total quantity sold, total revenue generated categorized by pizza name - Bottom 5 by quantity sold */
 SELECT pizza_name,
@@ -177,7 +177,7 @@ SELECT pizza_name,
 FROM pizzasales
 GROUP BY pizza_name
 ORDER BY total_pizza_sold ASC LIMIT 5;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated categorized by pizza name - Bottom 5 by quantity sold"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q14. What is the total orders, total quantity sold, total revenue generated categorized by pizza name - TOP 5 by orders placed */
 SELECT pizza_name,
@@ -187,7 +187,7 @@ SELECT pizza_name,
 FROM pizzasales
 GROUP BY pizza_name
 ORDER BY total_orders_placed DESC LIMIT 5;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated categorized by pizza name - TOP 5 by orders placed"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
 
 /* Q15. What is the total orders, total quantity sold, total revenue generated categorized by pizza name - Bottom 5 by orders placed */
 SELECT pizza_name,
@@ -197,4 +197,4 @@ SELECT pizza_name,
 FROM pizzasales
 GROUP BY pizza_name
 ORDER BY total_orders_placed ASC LIMIT 5;
--- ANSWER -- exported to a csv file with name "total orders, total quantity sold, total revenue generated categorized by pizza name - Bottom 5 by orders placed"
+-- ANSWER -- exported to a MICROSOFT WORD file with name "Pizza_Sales_MySQL_Query_results.docx"
